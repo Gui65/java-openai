3ac4719ef0e4a51add5abcf611cc3078699942ac
Guilherme Matos de Carvalho <guilhermematos851@gmail.com>
2024-02-08 20:20:10 -0300

    Criando um categorizador

---
+++ src/main/java/br/com/alura/ecommerce/CategorizadorDeProdutos.java
@@ -0,0 +1,52 @@
+package br.com.alura.ecommerce;
+
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+public class CategorizadorDeProdutos {
+
+    public static void main (String[] args){
+        var user = "Eletronicos";
+        var system = """
+                Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
+                
+                Escolha uma categoria dentra a lista abaixo:
+                
+                1. Higiene pessoal
+                2. Eletronicos
+                3. Esportes
+                4. Outros
+                
+                ##### exemplo de uso:
+                
+                Pergunta: Bola de Futebol
+                Resposta: Esportes
+                """;
+
+        var chave = System.getenv("OPENAI_API_KEY");
+        var service = new OpenAiService(chave, Duration.ofSeconds(30));
+
+        var completionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-3.5-turbo")
+                .messages(Arrays.asList(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
+                ))
+                .n(5)
+                .build();
+
+        service.createChatCompletion(completionRequest)
+                .getChoices()
+                .forEach(c -> {
+                    System.out.println(c.getMessage().getContent());
+                    System.out.println("-------------------------------");
+                });
+    }
+
+}
